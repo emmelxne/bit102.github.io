8968806ccaa1bd92d15d4ecf1d02a91004e37726
zwz0304 <1605203468@qq.com>
2025-03-13 20:48:47 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('show');
-}
+let isScrolling;
+
+window.addEventListener("scroll", function () {
+    // 隐藏导航栏
+    document.querySelector("header").style.top = "-60px";
+    document.querySelector("nav").style.top = "-60px";
+
+    // 清除之前的定时器
+    clearTimeout(isScrolling);
+
+    // 设定定时器，在停止滚动 500ms 后恢复显示
+    isScrolling = setTimeout(() => {
+        document.querySelector("header").style.top = "0";
+        document.querySelector("nav").style.top = "60px";
+    }, 500);
+});
